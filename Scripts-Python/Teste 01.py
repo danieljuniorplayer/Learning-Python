087d0f60f577485b252fc53de3ef97ab2f810bc3
Daniel Cruz <danieljunior.gd3@gmail.com>
2023-06-06 23:09:31 -0400

    commit inicial

---
+++ Scripts-Python/Teste 01.py
@@ -0,0 +1,10 @@
+nome=input('Qual é seu nome?')
+idade=input('Qual é a sua idade?')
+peso=input('Qual é o seu peso?')
+print(nome, idade, peso)
+
+n1=int(input('Insira o primeiro numero'))
+n2=int(input('Insira o segundo numero'))
+soma=n1+n2
+
+print('A soma de',n1,'mais',n2,'é igual a ',soma)
