45b01a782953352b8eae09a3268c99fdbad39d10
Daniel Cruz <danieljunior.gd3@gmail.com>
2023-06-07 21:25:11 -0400

    organizacao

---
+++ Curso Online - Gustavo Guanabara/pythonExercicios/while True.py
@@ -0,0 +1,11 @@
+while True:
+    cont=int(input('1-continuar\n0-parar'))
+
+    if cont==1:
+        print('parabens, o sistema vai continuar')
+        print('codigos e mais codigos')
+    elif cont==0:
+        print('Sistema finalizado')
+        break
+else:
+    print('voce digitou um valor diferente de 1 e 0, por favor, escolha entre 1 e 0')
