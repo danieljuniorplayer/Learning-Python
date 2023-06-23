4f32b16a367a0471733de88a3fc8667aa1885aab
Daniel Cruz <danieljunior.gd3@gmail.com>
2023-06-23 16:12:39 -0400

    up

--- Curso Online - Gustavo Guanabara/Mundo 1/Exercicios/Exer33 - maior e menor valor.py
+++ Curso Online - Gustavo Guanabara/Mundo 1/Exercicios/Exer33 - maior e menor valor.py
@@ -4,13 +4,20 @@ c = int(input('Digite o terceiro valor'))
 
 if a>=b and a>=c:
     print(f'{a} é o maior valor')
-    if b>c:
+    if c<b:
         print(f'{c} é o menor valor')
     else:
         print(f'{b} é o menor valor')
-else:
-    if b>c:
-        print(f'{b} é o maior valor')
+elif b>=c and b>=a:
+    print(f'{b} é o maior valor')
+    if a<c:
+        print(f'{a} é o menor valor')
     else:
-        print(f'{c} é o maior valor')
-    print(f'{a} é o menor valor')
+        print(f'{c} é o menor valor')
+else:
+    print(f'{c} é o maior valor')
+    if b<a:
+        print(f'{b} é o menor valor')
+    else:
+        print(f'{a} é o menor valor')
+
