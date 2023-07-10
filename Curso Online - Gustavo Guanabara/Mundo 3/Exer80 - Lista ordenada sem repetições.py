5b4d77f415e26306e8eb7bfc6c283a851ab8040e
Daniel Cruz Gomes Junior <110920426+danieljuniorplayer@users.noreply.github.com>
2023-07-10 17:56:43 -0400

    Atualizar o Exer80 - Lista ordenada sem repetições.py

--- Curso Online - Gustavo Guanabara/Mundo 3/Exer80 - Lista ordenada sem repetições.py
+++ Curso Online - Gustavo Guanabara/Mundo 3/Exer80 - Lista ordenada sem repetições.py
@@ -1,19 +1,22 @@
-lista = []for c in range(0,10): a = int(input('Digite um valor: ')) if c==0:        lista.append(a) elif c==1:      if lista[0] < a:            lista.append(a)     elif lista[0] >a:           lista.insert(c-1,a) elif c>1:       for d in range(0,len(lista)-1 ):            if lista[d] < a < lista[d+1]:               lista.insert(d+1,a)         elif lista[d] > a:              lista.insert(d,a)           elif a > max(lista):                lista.append(a)         breakprint(lista)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+lista = []
+for c in range(0,10):
+	a = int(input('Digite um valor: '))
+	if c==0:
+		lista.append(a)
+	elif c==1:
+		if lista[0] < a:
+			lista.append(a)
+		elif lista[0] >a:
+			lista.insert(c-1,a)
+	elif c>1:
+		for d in range(0,len(lista)-1 ):
+			if lista[d] < a < lista[d+1]:
+				lista.insert(d+1,a)
+			elif lista[d] > a:
+				lista.insert(d,a)
+			elif a > max(lista):
+				lista.append(a)
+			break
+print(lista)
 
 
